79a0f421c71a1f2740bbcdcdd18a58639ea5c12a
theMstafa <mm22180481@gmail.com>
2022-10-15 00:18:11 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // const new_task= document.querySelector('#new-task-description')
+  document.querySelector('#create-task-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleTodo(e.target.new_task.value)
+    document.querySelector('#create-task-form').reset()
+  })
 });
+function handleTodo(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click',deleteHandle)
+  btn.textContent = 'X'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+}
+function deleteHandle(e){
+  e.target.parentNode.remove()
+};
